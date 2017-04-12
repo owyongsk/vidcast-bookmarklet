c6038c11c554991701b28ab5079b6ec46368cc1f
SoonKhen OwYong <dude@owyong.sk>
2017-04-12 20:41:57 +0800

    adds more website for finding the video source

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,24 +1,43 @@
 /*
  * Uses vidcast.dabble.me to Chromecast
+ *
+ * or outputs the URL to download the mp4
  */
 
-var video = "";
-if (-1 != window.location.href.indexOf("putlockers.ch")) {
+var href = window.location.href;
+
+if (-1 != href.indexOf("putlocker")) {
   window.location = document.getElementsByTagName("iframe")[3].src;
 }
-if (-1 != window.location.href.indexOf("kisscartoon.se")) {
+if (-1 != href.indexOf("kisscartoon")) {
   video = document.getElementById("my_video_1_html5_api").src;
 }
-if (-1 != window.location.href.indexOf("sezonlukdizi.net")) {
-  var sources = document.getElementById("ms0007-drive").contentWindow.video;
-  if (sources[0].label == 1080) {
+if (-1 != href.indexOf("kissanime")) {
+  video = document.getElementById("my_video_1_html5_api").src;
+}
+if (-1 != href.indexOf("cartoonhd.online")) {
+  video = document.getElementById("flixvideostream_html5_api").src;
+}
+if (-1 != href.indexOf("yesmovies.to")) {
+  video = document.getElementsByClassName("jw-video")[0].src;
+}
+if (-1 != href.indexOf("sezonlukdizi.net")) {
+  var sources = document.getElementsByTagName("iframe")[1].contentWindow.video;
+  if (sources) {
     video = sources[0].file;
   }
 }
-if (-1 != window.location.href.indexOf("thevideos.tv")) {
-  video = jwplayer().config.sources[2].file;
+if (-1 != href.indexOf("thevideos.tv")) {
+  sources = jwplayer().config.sources;
+  video = sources[sources.length-1].file;
 }
 if (video) {
-  window.location = "https://vidcast.dabble.me/index.html?video_link="
-    + encodeURIComponent(video.replace(/&amp;/g, '&'));
+  if (window.sk_vid_cast) {
+    window.location = "https://vidcast.dabble.me/index.html?video_link="
+      + encodeURIComponent(video.replace(/&amp;/g, '&'));
+  } else {
+    alert(video);
+  }
+} else {
+  alert("Can't find an mp4 here!");
 }
